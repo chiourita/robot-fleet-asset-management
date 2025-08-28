12df9961d96e476b4949169fa88c374317bd8ec8
chiourita <chiourita@gmail.com>
2025-08-28 23:17:09 +0300

    Add Prometheus monitoring setup

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,3 +29,46 @@ resource "docker_network" "robot_network" {
   }
 }
 
+resource "docker_container" "prometheus" {
+  count = var.enable_monitoring ? 1 : 0
+  
+  name  = "prometheus"
+  image = "prom/prometheus:latest"
+  
+  ports {
+    internal = 9090
+    external = 9090
+  }
+  
+  volumes {
+    host_path      = abspath("${path.module}/prometheus.yml")
+    container_path = "/etc/prometheus/prometheus.yml"
+    read_only      = true
+  }
+  
+  networks_advanced {
+    name = docker_network.robot_network.name
+  }
+  
+  restart = "unless-stopped"
+
+  labels {
+    label = "managed-by"
+    value = "terraform"
+  }
+  
+  labels {
+    label = "service"
+    value = "monitoring"
+  }
+}
+
+# Generate Prometheus config file for robot monitoring if enabled
+resource "local_file" "prometheus_config" {
+  count = var.enable_monitoring ? 1 : 0
+  
+  filename = "${path.module}/prometheus.yml"
+  content = templatefile("${path.module}/prometheus.yml.tpl", {
+    robot_targets = ["robot_a:8001", "robot_b:8002", "robot_c:8003"]
+  })
+}
